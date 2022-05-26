cf3f1ffce8b3ce529eb0d7f7c7067767541072c5
eaghayev <eaghayevv@gmail.com>
2022-05-26 22:41:36 +0400

    project added

---
+++ FirstWeb/FirstWeb/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWeb.Controllers
+{
+    public class HomeController:Controller
+    {
+        //public ContentResult Index()
+        //{
+        //    var content =  Content("Salam");
+        //    return content;
+        //}
+
+        public ViewResult Index()
+        {
+            //var view = new ViewResult();
+            //view.ViewName = "Index";
+            //return view;
+
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            var view = new ViewResult();
+            view.ViewName = "About";
+            return view;
+        }
+    }
+}
